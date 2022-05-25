b73eba52cc9225e51d5c441db71b069af44afbc5
gediminas <gedas.fs@gmail.com>
2022-05-25 22:04:01 +0300

    JavaScript tasks. +strings

--- week4/tasks_js/js/app.js
+++ week4/tasks_js/js/app.js
@@ -7,7 +7,7 @@ let num2 = +prompt('Enter Number 2:');
 console.log('Sum: ', num1 + num2);
 */
 
-
+//----------------------------------
 
 /*
 Sukurkite kodą, kuris sukurs penkis atsitiktinius skaičius (skaičiai negali būti didesni nei 10) ir juos priskirs kintamiesiems, kurie turi iki dviejų skaičių po kablelio;
@@ -16,7 +16,6 @@ Tada suraskite, kuris iš sukurtų skaičių yra:
 didžiausias;
 mažiausias.
 */
-
 
 /*
 let a = parseFloat((Math.random() * 10).toFixed(2));
@@ -55,3 +54,32 @@ for (let i=0; i<5; i++) {
 console.log(numArr);
 console.log('Max: ', Math.max(...numArr));
 console.log('Min: ', Math.min(...numArr));
+
+//----------------------------------
+
+let str1 = 'Hello';
+let str2 = "world";
+let str3 = `!`;
+
+console.log(`String: ${str1}`);
+
+let strWithNewLine = `1st line
+2nd line 
+
+!`;
+
+console.log(strWithNewLine); 
+
+//-----------------------------------
+
+/*
+Naudodami dvigubas (‘’ arba “”) kabutes, console.log metodo viduje parašykite tokius sakinius:
+	I am a "double quoted" string inside "double quotes„;
+FirstLine;
+\SecondLine;
+ThirdLine.
+*/
+
+console.log("\tI am a \"double quoted\" string inside \"double quotes\„; \nFirstLine; \n\\SecondLine; \nThirdLine.");
+
+//-----------------------------------
