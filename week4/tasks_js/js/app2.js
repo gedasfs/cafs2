c7b35f59a381d3187f2ac3023871dfa5e64e2517
gediminas <gedas.fs@gmail.com>
2022-05-26 21:56:13 +0300

    js tasks

--- week4/tasks_js/js/app2.js
+++ week4/tasks_js/js/app2.js
@@ -26,5 +26,55 @@ console.log(`division = ${division};`);
 
 
 /*
-
+Parašykite if...else sąlygą, kuri: 	
+consolė’je atspausdins lyginis“, jei skaičius yra lyginis skaičius;
+consolė’je atspausdins „nelyginis“, jei skaičius yra nelyginis skaičius.
+Įsitikinkite, kad išbandėte savo kodą su skirtingomis vertėmis.
+Pvz.: Jei skaičius lygus 1, tada konsolėje turėtų būti atspausdinama “nelyginis”.
 */
+
+/*
+let num = +prompt('Your number');
+if ((num % 2) == 0) {
+	console.log('The number is even');
+} else {
+	console.log('The number is odd.');
+}
+*/
+// -----------------------------------------------------------------------
+
+
+/*
+Muzikinės grupės turi specialius pavadinimus pagal žmonių skaičių grupėje. Pvz., „kvartetas“ yra muzikinė grupė, kurią sudaro keturi muzikantai.
+Parašykite sąlyginių teiginių seriją (else...if statements), kuri:	
+Atspausdins consolėje "ne grupė" jei muzikantų yra mažiau ar lygu 0;	
+Atspausdins consolėje "solo" jei muzikantų yra 1;	
+Atspausdins consolėje "duetas" jei muzikantų yra 2;	
+Atspausdins consolėje "trio" jei muzikantų yra 3;	
+Atspausdins consolėje "kvartetas" jei muzikantų yra 4;	
+Atspausdins consolėje "didelė grupė" jei muzikantų yra daugiau nei 4.
+Įsitikinkite, kad išbandėte savo kodą su skirtingomis vertėmis.
+Pvz., Jeigu muzikantų yra 3, tada "trio" turi būti atspausdinama consolėje.
+*/
+/*
+let num = +prompt('Žmonių skaičius grupėje: ');
+
+if (num == 0) {
+	console.log('ne grupė');
+} else if (num == 1) {
+	console.log('solo');
+} else if (num == 2) {
+	console.log('duetas');
+} else if (num == 3) {
+	console.log('trio');
+} else if (num == 4) {
+	console.log('kvartetas');
+} else if (num > 4) {
+	console.log('didelė grupė');
+} else {
+	console.log('Įvedėte neigiamą skaičių.');
+}
+*/
+
+
+
