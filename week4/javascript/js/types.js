04add78190ca44792356a026faf91ad7fcf3895a
gediminas <gedas.fs@gmail.com>
2022-05-26 17:55:40 +0300

    js tasks. renamed files

---
+++ week4/javascript/js/types.js
@@ -0,0 +1,22 @@
+console.log("Hello world.");
+
+console.log(true, typeof true);
+console.log(false, typeof false);
+
+console.log(null, typeof null);
+
+console.log(Number, typeof Number);
+console.log(50, typeof 50);
+console.log(NaN, typeof NaN);
+console.log(4/'a', typeof 4/'a');
+console.log(1.23, typeof 1.23);
+
+console.log(String, typeof String);
+console.log('a', typeof 'a');
+console.log('', typeof '');
+console.log(' ', typeof ' ');
+
+console.log(Object, typeof Object);
+console.log({}, typeof {});
+console.log(Array, typeof Array);
+console.log([], typeof []);
