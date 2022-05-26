5d2a33dccab40c862bb1029a73d8dfe7a75734f8
gediminas <gedas.fs@gmail.com>
2022-05-26 19:33:29 +0300

    fixed backtips to '

--- week4/tasks_js/strings/script.js
+++ week4/tasks_js/strings/script.js
@@ -1,3 +1,13 @@
+/*
+Sukurkime aplanką pavadinimu “Strings” ir jame inicijuokime du failus index.html ir script.js.
+Index.html faile patalpinime startinį kodą, kuriame būtų pirminiai, privalomi tag’ai (<html>, <head>, <body> su privalomais child tag’ai);
+Index.html failo <body> tag’o viduje susikurkime tuščią <div> turintį id=”main”;
+index.html ir script.js susiekime „External JavaScript“ būdu (prieš <head> arba </body> tag’us);
+Atsidarome script.js failą ir sukuriame kintamąjį “userInfoOutput”, kuriam priskiriame index.html faile esantį <div> su id=”main” (naudokite: document.getelementbyid());
+Susikurkite objektą “user”, kuriam nurodykite šias savybes: vardas, pavardė;
+Kintamąjam userInfoOutput naudojant “.innerHTML =” DOM nuosavybę ir atgalinius klavišus (backticks) t.y. ``, sukurkite H2 tag, kuris, nurodo “User Information” ir <p> tag’ą, kuriame pateiktas, toks sakinys “Vartotojo vardas yra (vardas), o pavardė (pavardę).”
+*/
+
 let userInfoOutput = document.getElementById('main');
 
 let user = {
@@ -6,5 +16,5 @@ let user = {
 };
 
 userInfoOutput.innerHTML = 
-	`<h2>User Information</h2>` +
+	'<h2>User Information</h2>' +
 	`<p>Vartotojo vardas yra ${user.name}, o pavardė ${user.lastname}.</p>`;
