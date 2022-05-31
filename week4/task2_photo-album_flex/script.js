576dbcd9868a754ff21ebe487e2712670a1ab41e
Gediminas S <gedas.fs@gmail.com>
2022-05-31 13:55:48 +0000

    task2. gallery w/ flexbox. +media queries. added nav-icons

--- week4/task2_photo-album_flex/script.js
+++ week4/task2_photo-album_flex/script.js
@@ -1,10 +1,12 @@
-const navHamburger = document.querySelector('.nav-hamburger');
+const navHamburger = document.querySelector('.nav-hamburger-cont');
 const barIcon = document.querySelector('.bi-list');
 const closeIcon = document.querySelector('.bi-x');
 const navList = document.querySelector('.ul-nav-links');
+const navButton = document.querySelector('.nav-btn-cont');
 
 navHamburger.addEventListener('click', function() {
-  barIcon.classList.toggle('btn-active');
+  barIcon.classList.toggle('burger-active');
   navList.classList.toggle('nav-active')
-  closeIcon.classList.toggle('btn-active');
+  closeIcon.classList.toggle('burger-active');
+  navButton.classList.toggle('nav-active');
 });
