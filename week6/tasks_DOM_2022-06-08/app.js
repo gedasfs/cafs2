67ffc02eee1a934579310b807151acca449c1c0f
Gediminas <gedas.fs@gmail.com>
2022-06-09 21:31:21 +0300

    folder name changed

---
+++ week6/tasks_DOM_2022-06-08/app.js
@@ -0,0 +1,19 @@
+const ageTable = document.getElementById('age-table');
+console.log('Table with id="age-table": ', ageTable);
+
+const tdAge = document.getElementById('age');
+console.log('td with id="age": ', tdAge);
+
+
+const allLabels = document.querySelectorAll('table label');
+if (allLabels.length > 0 ) {
+	console.log('All label elements: ');
+	for (let el of allLabels) {
+		console.log(el);
+	}
+}
+
+
+const inputs = document.querySelectorAll('form > input');
+console.log('first input: ', inputs[0]);
+console.log('last input: ', inputs[inputs.length-1]);
