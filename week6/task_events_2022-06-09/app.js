740440fd756c00c468fc5ca73dd866b971e5a026
Gediminas <gedas.fs@gmail.com>
2022-06-09 21:47:47 +0300

    week6, task events

---
+++ week6/task_events_2022-06-09/app.js
@@ -0,0 +1,9 @@
+const inp1 = document.querySelector('.inp1');
+const inp2 = document.querySelector('.inp2');
+const btn = document.querySelector('button');
+
+
+btn.addEventListener('click', (event) => {
+    inp2.value = inp1.value;
+    inp1.value = '';
+});
