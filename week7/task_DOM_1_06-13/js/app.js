21ed14ff96b96e59f09f849906b664c9b07c374e
Gediminas S <gedas.fs@gmail.com>
2022-06-13 17:42:31 +0000

    week7, DOM task 1 (window resize event). Added set function

--- week7/task_DOM_1_06-13/js/app.js
+++ week7/task_DOM_1_06-13/js/app.js
@@ -1,3 +1,4 @@
+/*
 const outputH1 = document.querySelector('h1');
 const x0 = window.innerWidth;
 const y0 = window.innerHeight;
@@ -15,4 +16,27 @@ if (outputH1) {
 		outputH1.textContent = getMsgWithXY(window.innerWidth, window.innerHeight);
 	});
 }
+*/
+// ------------------------------
+
+const outputH1 = document.querySelector('h1');
+
+function getMsgWithXY(x, y) {
+	let msg = `x: ${x}, y: ${y}`;
+
+	return msg;
+}
+
+function setH1TextContent (outputElement) {
+	outputElement.textContent = getMsgWithXY(window.innerWidth, window.innerHeight);
+}
+
+if(outputH1) {
+	setH1TextContent(outputH1);
+
+	window.addEventListener('resize', function () {
+		setH1TextContent(outputH1);
+	});
+}
+ 
 
