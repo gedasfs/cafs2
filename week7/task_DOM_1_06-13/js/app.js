02a140c2a329fcf81183fe942e935620b9c4f163
Gediminas S <gedas.fs@gmail.com>
2022-06-13 16:52:09 +0000

    week7, DOM task 1 (window resize event)

--- week7/task_DOM_1_06-13/js/app.js
+++ week7/task_DOM_1_06-13/js/app.js
@@ -1,6 +1,6 @@
 const outputH1 = document.querySelector('h1');
-let x0 = window.innerWidth;
-let y0 = window.innerHeight;
+const x0 = window.innerWidth;
+const y0 = window.innerHeight;
 
 if (outputH1) {
 	outputH1.textContent = `x: ${x0}, y: ${y0}`;
