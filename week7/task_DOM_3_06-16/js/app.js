0c4ef87dccfc8be012d766fa89128191f6b3b4d2
Gediminas <gedas.fs@gmail.com>
2022-06-16 21:50:16 +0300

    week7, DOM task (functions added)

--- week7/task_DOM_3_06-16/js/app.js
+++ week7/task_DOM_3_06-16/js/app.js
@@ -2,26 +2,58 @@ window.addEventListener('DOMContentLoaded', () => {
     const divMain = document.querySelector('#main');
     
     const header = document.createElement('h2');
-    const textHeader = document.createTextNode('Header text');
-    header.appendChild(textHeader);
+    createAppendTextNode(header, 'Header text');
     divMain.appendChild(header);
+
+    // const header = document.createElement('h2');
+    // const textHeader = document.createTextNode('Header text');
+    // header.appendChild(textHeader);
+    // divMain.appendChild(header);
     
+
+
     const pFirst = document.createElement('p');
-    const textPFirst = document.createTextNode('Paragraph text');
-    pFirst.appendChild(textPFirst);
+    createAppendTextNode(pFirst, 'Paragraph text');
     divMain.appendChild(pFirst);
+
+    // const pFirst = document.createElement('p');
+    // const textPFirst = document.createTextNode('Paragraph text');
+    // pFirst.appendChild(textPFirst);
+    // divMain.appendChild(pFirst);
    
+
+
     const listUl = document.createElement('ul');
     const listLi = document.createElement('li');
-    const textLi = document.createTextNode('Li element text');
+    createAppendTextNode(listLi, 'Li element text');
     listUl.appendChild(listLi);
-    listLi.appendChild(textLi);
     divMain.appendChild(listUl);
 
-    const psecond = document.createElement('p');
-    const textPSecond = document.createTextNode('Paragraph text second');
-    psecond.appendChild(textPSecond);
-    divMain.appendChild(psecond);
+    // const listUl = document.createElement('ul');
+    // const listLi = document.createElement('li');
+    // const textLi = document.createTextNode('Li element text');
+    // listUl.appendChild(listLi);
+    // listLi.appendChild(textLi);
+    // divMain.appendChild(listUl);
+
+
+    const pSecond = document.createElement('p');
+    createAppendTextNode(pSecond, 'Paragraph text second');
+    divMain.appendChild(pSecond);
+    
+    // const pSecond = document.createElement('p');
+    // const textPSecond = document.createTextNode('Paragraph text second');
+    // psecond.appendChild(textPSecond);
+    // divMain.appendChild(psecond);
 
     // console.log(divMain);
 });
+
+function createParagr(child, parent) {
+
+}
+
+function createAppendTextNode (parent, text) {
+    const textNode = document.createTextNode(text);
+    parent.appendChild(textNode);
+}
