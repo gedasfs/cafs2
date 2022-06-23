f2a3bd05d0e6f2e0875198a7f7615cf88c0a7e49
Gediminas S <gedas.fs@gmail.com>
2022-06-23 18:26:36 +0000

    week8, oop task Movies

--- week8/task_oop_movies_06-23/js/classes/Movie.js
+++ week8/task_oop_movies_06-23/js/classes/Movie.js
@@ -1,3 +1,12 @@
+/*
+Naudojant ES6 klases sukurkite objekto Filmas (Movie) kūrimo konstruktorių, kuris turės šias savybes: 
+1. name; 2. year; 3. director; 4. budget; 5. income.
+ir metodus: 
+1. getIntroduction, kuris grąžins filmo pilną pavadinimą (su name, year, director); 
+2. getProfit, kuris grąžins sumą, kurią uždirbo (pelną) filmas.
+Testavimui, sukurkite du filmus, kurie turės nurodytas savybes ir metodus.
+*/
+
 class Movie {
     constructor(name, year, director, budget, income) {
         this.name = name;
