ca124656416ecef194c97395674166952fb7c1c3
GedasFS <gedas.fs@gmail.com>
2022-07-18 18:56:37 +0300

    week11, task: nodejs http + frontend

---
+++ week12/task_nodejs+frontend_07-18/script.js
@@ -0,0 +1,115 @@
+// EXTERNAL API
+const JSONPLACEHOLDER_URI = 'https://jsonplaceholder.typicode.com/posts?limit=10';
+
+// *** Variables ***
+//-- buttons
+const btnGetTextFile = document.getElementById('btn1');
+const btnGetUser = document.getElementById('btn2');
+const btnGetUsers = document.getElementById('btn3');
+const btnGetPosts = document.getElementById('btn4');
+const btnSendPost = document.getElementById('btn5');
+//-- output
+const textOutput = document.querySelector('#text');
+const userOutput = document.querySelector('#user');
+const usersOutput = document.querySelector('#users');
+const postsOutput = document.querySelector('#posts');
+
+// *** Functions ***
+//OLD Version AJAX (XMLHttpRequest())
+//-- Load Text File Information
+
+function getDataFrom(source) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', source);
+
+    xhr.addEventListener('load', () => {
+      if (xhr.status === 200) {
+        console.log(xhr);
+        resolve(xhr.response);
+      } else {
+        reject(xhr.responseText);
+      }
+    });
+
+    xhr.addEventListener('error', () => {
+      reject(xhr.responseText);
+    });
+
+    xhr.send();
+  });
+}
+
+function loadTextFileXHR() {
+  getDataFrom('http://localhost:8080/sample')
+    .then(response => {
+      textOutput.textContent = response;
+      console.log(response);
+    })
+    .catch(error => console.log(error));
+}
+
+//-- Load User Information
+function loadUserXHR() {
+  getDataFrom('http://localhost:8080/user')
+    .then(response => JSON.parse(response))
+    .then(data => {
+      userOutput.textContent = `${data.name}, ${data.email}`;
+      console.log(data);
+    })
+    .catch(error => console.log(error));
+}
+
+//-- Load Users information
+function loadUsersXHR() {
+  getDataFrom('http://localhost:8080/users')
+    .then(response => JSON.parse(response))
+    .then(dataArr => {
+
+      dataArr.forEach(data => {
+        usersOutput.innerHTML += `<p>${data.name}, ${data.email}</p>`;
+        console.log(data);
+      })
+    })
+    .catch(error => console.log(error));
+}
+
+//-- Load Users information
+function loadPostsXHR() {
+  return;
+}
+
+//NEW VERSION AJAX (fetch())
+// -- Getting data
+function loadPostsFETCH() {
+  return;
+}
+
+// -- Sending data
+function sendPostFETCH() {
+  return;
+}
+// *** Events ***
+btnGetTextFile.addEventListener('click', loadTextFileXHR);
+btnGetUser.addEventListener('click', loadUserXHR);
+btnGetUsers.addEventListener('click', loadUsersXHR);
+btnGetPosts.addEventListener('click', loadPostsXHR);
+btnGetPosts.addEventListener('click', loadPostsFETCH);
+btnSendPost.addEventListener('click', sendPostFETCH);
+
+/*
+    readyState Values:
+    0: request not initialized
+    1: server connection established
+    2: request received
+    3: processing request
+    4: request finished and response is ready
+    More: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+
+    HTTP Statuses
+    200: "OK"
+    403: "Forbidden"
+    404: "Not Found"
+    More: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+*/
---
+++ week12/task_nodejs+frontend_07-18/server.js
@@ -0,0 +1,70 @@
+const fs = require('node:fs');
+const http = require('node:http');
+
+const generateFeedback = function(filePath, responseObj) {
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            console.log('Error reading file: ', error);
+            responseObj.writeHead(404, setContentTypeByExt());
+            responseObj.end(`Resource not found.`);
+        } else {
+            let ext = getFileExt(filePath);
+            console.log(`File ${filePath} read ok.`);
+            responseObj.writeHead(200, setContentTypeByExt(ext));
+            responseObj.end(content);
+        }
+    });
+    
+}
+
+const getFileExt = function(fileName) {
+    return fileName.includes('.') ? fileName.split('.').pop() : '';
+}
+
+const setContentTypeByExt = function(ext = '') {
+    let contentType = {'Content-Type': 'text/html',};
+    
+    switch (ext) {
+        case 'css':
+            contentType['Content-Type'] = 'text/css'
+            break;
+        case 'js':
+            contentType['Content-Type'] = 'application/javascript'
+            break;    
+        case 'json':
+            contentType['Content-Type'] = 'application/json'
+            break;
+        case 'txt':
+            contentType['Content-Type'] = 'text/plain'
+            break;
+        case '':
+            contentType['Content-Type'] = 'text/plain'
+            break;
+    }
+
+    return contentType;
+}
+
+http.createServer(function(request, response) {
+    console.log(request.url);
+    
+    if (request.url === '/' || request.url === '/index.html' || request.url === '/index') {
+        generateFeedback('index.html', response);
+    }
+    else if (request.url === '/script.js') {
+        generateFeedback('script.js', response);
+    }
+    else if (request.url === '/sample') {
+        generateFeedback('sample.txt', response);
+    }
+    else if (request.url === '/user') {
+        generateFeedback('user.json', response);
+    }
+    else if (request.url === '/users') {
+        generateFeedback('users.json', response);
+    }
+    else {
+        response.writeHead(404, setContentTypeByExt());
+        response.end('Page not found.');
+    }    
+}).listen(8080, () => console.log('Listening on port 8080.'));
