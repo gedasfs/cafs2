baf2d4c1047490b22f27a869db6fd347ba73d13d
GedasFS <gedas.fs@gmail.com>
2022-07-19 22:19:17 +0300

    added root (/) route

--- week12/task_expressjs_07-19/app.js
+++ week12/task_expressjs_07-19/app.js
@@ -5,10 +5,26 @@ const app = express();
 const os = require('node:os');
 
 app
+.get('/', function(req, res) {
+    let routeStack = app._router.stack;
+    let resContent = '';
+
+    for (const key in routeStack) {
+        if (routeStack[key].route && routeStack[key].route.path) {
+            let path = routeStack[key].route.path;
+            
+            resContent += `<p><a href="${path}">${path}</a></p>`;
+        }
+    }
+
+    res.send(resContent);
+})
 .get('/cpu', function(req, res) {
     let cpu = {
         model: os.cpus()[0].model,
+        cores: os.cpus().length,
     };
+
     res.send(JSON.stringify(cpu));
 })
 .get('/ram', function(req, res) {
@@ -16,13 +32,15 @@ app
         bytes: os.totalmem(),
         GB: os.totalmem() / (1E9),
     };
+
     res.send(JSON.stringify(ram));
 })
 .get('/os', function(req, res) {
     let opSys = {
         platform: os.platform(),
-        version: os.release(),
+        release: os.release(),
     };
+
     res.send(JSON.stringify(opSys));
 });
 
