a4075137a2346a4feaea5ac2cc53b3c62eac7915
Gediminas <gedas.fs@gmail.com>
2022-08-08 20:43:50 +0300

    fixes

--- BackEnd/src/week15/tasks/forms/index.php
+++ BackEnd/src/week15/tasks/forms/index.php
@@ -1,31 +1,28 @@
 <?php
-    require_once './configs/index_form_vars.php';
-    $messages = [];
+require_once './configs/index_form_vars.php';
+$messages = [];
 
-    if (count($_POST)) {
-        if (!isset($_POST['reg_form']) || $_POST['reg_form'] !== 'submitted') {
-            $messages['error'] = true;
-            $messages['msgStr'] = 'Neaiškus formos išsiuntimo šaltinis. Jūsų informacija nebuvo išsaugota.';
-        } else {
-            if ((!isset($_POST['name']) || $_POST['name'] == '') || (!isset($_POST['lastname']) || $_POST['lastname'] == '')) {
-                $messages['error'] = true;
-                $messages['msgStr'] = 'Būtina užpildyti vardo ir pavardės laukelius.';
-            } elseif (!isset($_POST['coding_lang']) || !count($_POST['coding_lang'])) {
-                $messages['error'] = true;
-                $messages['msgStr'] = 'Būtina pasirinkti bent vieną programavimo kalbą.';
-            } else {
-                $messages['success'] = true;
-                $messages['msgStr'] = 'Jūsų registracija sėkminga.';
-            }
-        }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if ((!isset($_POST['name']) || $_POST['name'] == '') || (!isset($_POST['lastname']) || $_POST['lastname'] == '')) {
+        $messages['error'] = true;
+        $messages['msgStr'] = 'Būtina užpildyti vardo ir pavardės laukelius.';
+    } elseif (!isset($_POST['coding_lang']) || !count($_POST['coding_lang'])) {
+        $messages['error'] = true;
+        $messages['msgStr'] = 'Būtina pasirinkti bent vieną programavimo kalbą.';
+    } else {
+        $messages['success'] = true;
+        $messages['msgStr'] = 'Jūsų registracija sėkminga.';
     }
-    
+}
 
-    require_once './views/index.phtml';
 
-    if (isset($messages['success'])) {
-        echo '<pre>';
-        print_r($_POST);
-        echo '</pre>';
-    }
+
+require_once './views/index.phtml';
+
+if (isset($messages['success'])) {
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+}
     
