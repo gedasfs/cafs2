d3481c679dfee616a13ec22b977b40fb753e8160
GedasFS <gedas.fs@gmail.com>
2022-08-22 19:23:43 +0300

    added header checking

--- BackEnd/src/week16/tasks/file_io_08-18/index.php
+++ BackEnd/src/week16/tasks/file_io_08-18/index.php
@@ -1,8 +1,4 @@
 <?php
-
-$csvFilePath = 'excelFile.csv';
-$apiUrl = 'https://randomuser.me/api/';
-
 // https://stackoverflow.com/questions/526556/how-to-flatten-a-multi-dimensional-array-to-simple-one-in-php
 function flatten($array, $prefix = '') {
     $result = [];
@@ -18,23 +14,33 @@ function flatten($array, $prefix = '') {
     return $result;
 }
 
+$csvFilePath = 'excelFile.csv';
+$apiUrl = 'https://randomuser.me/api/';
+
 try {
     $data = json_decode(file_get_contents($apiUrl), true);
 
     if (isset($data) && array_key_exists('results', $data)) {
         $flatData = [];
-        $headerSaved = false;
+        $headersSaved = false;
 
         foreach ($data['results'] as $line) {
+            $line['#headers'] = '#data';
             $flatData[] = flatten($line);
         }
 
-        $file = fopen($csvFilePath, 'w');
+        $file = fopen($csvFilePath, 'a+');
+
+        $fileData = fgetcsv($file);
+
+        if (is_array($fileData) && in_array('#headers', $fileData)) {
+            $headersSaved = true;
+        }
 
         foreach ($flatData as $line) {
-            if (!$headerSaved) {
+            if (!$headersSaved) {
                 fputcsv($file, array_keys($line));
-                $headerSaved = true;
+                $headersSaved = true;
             }
 
             fputcsv($file, array_values($line));
