1d07baf290da6569db4bb635f6267e3010e97c72
Gediminas <gedas.fs@gmail.com>
2022-09-08 20:22:34 +0300

    php task. OOP extends

--- BackEnd/src/week19/tasks/OOP_classes_extend/index.php
+++ BackEnd/src/week19/tasks/OOP_classes_extend/index.php
@@ -14,7 +14,7 @@ $vehicles = [
     new Plane('Boeing', '777', 1995, 6),
     new Motorcycle('Suzuki', 'Katana', 1980, 2),
     new Car('Audi', 'A6', 2018, 4),
-    new Bmw(model: 'i4', year: 2018, wheels: 4),
+    new Bmw('i4', 2018, 4),
     new Bus('Toyota', 'Hiace', 2022, 4),
     new Boat('Sea Fox', 'Commander 368', 2020),
 ];
