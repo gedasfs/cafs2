d3827f0380cc35f203766a73bc9f4ada450f22e3
Gediminas <gedas.fs@gmail.com>
2022-09-15 22:17:08 +0300

    php tasks. OOP namespaces and traits

---
+++ BackEnd/src/week20/tasks/OOP_namespace_traits/src/Classes/FormBuilder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Classes;
+
+use App\Traits\HtmlTag;
+
+class FormBuilder
+{
+    use HtmlTag;
+    
+    public function open(string $action, string $method) : string
+    {
+        return $this->buildFormElement('form', ['action' => $action, 'method' => $method]);
+    }
+
+    public function close() : string
+    {
+        return $this->buildFormElement('/form');
+    }
+
+    public function label(string $idName, string $labelTxt) : string
+    {
+        return $this->buildFormElement('label', ['for' => $idName], $labelTxt, true, true);
+    }
+
+    public function input(string $type, ?string $placeholder = null, ?string $name = null, ?string $id = null) : string
+    {   
+        $attributes = $this->cleanArray(get_defined_vars());
+        $inputStr = $this -> buildFormElement('input', $attributes, '', false, true);
+
+        return $inputStr;
+    }
+
+    public function password(string $placeholder, ?string $name = null, ?string $idName = null) : string
+    {
+        return $this->input('password', $placeholder, $name, $idName);
+    }
+
+    public function textarea(?string $placeholder = null, ?string $name = null, ?string $idName = null, int $rows = null, ?int $cols = null) : string
+    {
+        $attributes = $this->cleanArray(get_defined_vars());
+        $inputStr = $this -> buildFormElement('textarea', $attributes, '', true, true);
+
+        return $inputStr;
+    }
+
+    public function submit(string $btnTxt) : string
+    {
+        return $this -> buildFormElement('button', [], $btnTxt, true, false);
+    }
+
+    public function checkbox(?string $name = null, ?string $id = null, bool $ckecked = false) : string
+    {
+        $attributes = $this->cleanArray(get_defined_vars());
+        $attributes['type'] = 'checkbox';
+
+        return $this->buildFormElement('input', $attributes);
+    }
+
+    private function cleanArray(array $arr)
+    {
+        return array_filter($arr, function ($v) {
+            return $v != null;
+        });
+    }
+}
---
+++ BackEnd/src/week20/tasks/OOP_namespace_traits/src/Traits/HtmlTag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+trait HtmlTag
+{
+    public function buildFormElement(string $tagName, array $attributes = [], string $elTxt = '', bool $closingTag = false, bool $breakLine = false)
+    {
+        $attributesStr = '';
+
+        foreach ($attributes as $name => $value) {
+            $attributesStr .= "{$name}=\"{$value}\" ";
+        }
+
+        $elementStr = "<{$tagName} {$attributesStr}>";
+
+        if ($closingTag) {
+            $elementStr .= "{$elTxt}";
+            $elementStr .= "</{$tagName}>";
+        }
+
+        if ($breakLine) {
+            $elementStr .= "<br>";
+        }
+
+        return $elementStr;
+    }
+}
