74b264e3044ee43bd56c7bc097bea92a2d8ce8ff
GedasFS <gedas.fs@gmail.com>
2022-09-16 09:24:38 +0300

    misc fixes (method visibility, method param names, etc.)

--- BackEnd/src/week20/tasks/OOP_namespace_traits/src/Classes/FormBuilder.php
+++ BackEnd/src/week20/tasks/OOP_namespace_traits/src/Classes/FormBuilder.php
@@ -26,17 +26,17 @@ class FormBuilder
     public function input(string $type, ?string $placeholder = null, ?string $name = null, ?string $id = null) : string
     {   
         $attributes = $this->cleanArray(get_defined_vars());
-        $inputStr = $this -> buildElement('input', $attributes, '', false);
+        $inputStr = $this -> buildElement('input', $attributes, '');
 
         return $inputStr;
     }
 
-    public function password(?string $placeholder = null, ?string $name = null, ?string $idName = null) : string
+    public function password(?string $placeholder = null, ?string $name = null, ?string $id = null) : string
     {
-        return $this->input('password', $placeholder, $name, $idName);
+        return $this->input('password', $placeholder, $name, $id);
     }
 
-    public function textarea(?string $placeholder = null, ?string $name = null, ?string $idName = null, int $rows = null, ?int $cols = null) : string
+    public function textarea(?string $placeholder = null, ?string $name = null, ?string $id = null, int $rows = null, ?int $cols = null) : string
     {
         $attributes = $this->cleanArray(get_defined_vars());
         $inputStr = $this -> buildElement('textarea', $attributes, '', true);
@@ -60,7 +60,6 @@ class FormBuilder
     {
         return $this -> buildElement('button', [], $btnTxt, true);
     }
-
 
     private function cleanArray(array $arr)
     {
--- BackEnd/src/week20/tasks/OOP_namespace_traits/src/Traits/HtmlTag.php
+++ BackEnd/src/week20/tasks/OOP_namespace_traits/src/Traits/HtmlTag.php
@@ -4,7 +4,7 @@ namespace App\Traits;
 
 trait HtmlTag
 {
-    public function buildElement(string $tagName, array $attributes = [], string $elTxt = '', bool $closingTag = false)
+    private function buildElement(string $tagName, array $attributes = [], string $elTxt = '', bool $closingTag = false)
     {
         $attributesStr = '';
 
