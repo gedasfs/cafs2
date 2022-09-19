9a56e8959fe501b3a003813bd0b9dc5b0338a0c2
Gediminas <gedas.fs@gmail.com>
2022-09-19 21:13:46 +0300

    task php. OOP method chaining

--- BackEnd/src/week21/tasks/OOP_method_chaining/public/index.php
+++ BackEnd/src/week21/tasks/OOP_method_chaining/public/index.php
@@ -12,11 +12,22 @@ echo $tagA->setText('text2')->setAttr('href', 'index2.html')->setAttr('target', 
 echo "\n\n<br><br>";
 
 $form = new Tag('form');
-$formBody = (new Tag('label'))->setText('Label: name')->setAttr('id', 'name')->get();
+$formBody = (new Tag('label'))
+    ->setText('Label: name')
+    ->setAttr('id', 'name')
+    ->get();
 $formBody .= "\n<br>";
-$formBody .= (new Tag('input'))->setAttr('type', 'text')->setAttr('name', 'name')->setAttr('id', 'name')->setAttr('placeholder', 'your name')->get();
+$formBody .= (new Tag('input'))
+    ->setAttr('type', 'text')
+    ->setAttr('name', 'name')
+    ->setAttr('id', 'name')
+    ->setAttr('placeholder', 'your name')
+    ->get();
 $formBody .= "\n<br>";
-$formBody .= (new Tag('button'))->setAttr('type', 'submit')->setText('Go')->get();
+$formBody .= (new Tag('button'))
+    ->setAttr('type', 'submit')
+    ->setText('Go')
+    ->get();
 $form->setAttr('method', 'POST')->setAttr('action', 'index.php')->setText($formBody);
 $form->show();
 echo "\n<br>";
