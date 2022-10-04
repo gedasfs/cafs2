cc1a3ceb54076621cbefea2139a6a334bfa9c735
GedasFS <gedas.fs@gmail.com>
2022-10-04 18:38:21 +0300

    php task. PDO, get admins

--- BackEnd/src/week23/tasks/PDO_users/PdoAdmins.php
+++ BackEnd/src/week23/tasks/PDO_users/PdoAdmins.php
@@ -2,7 +2,7 @@
 
 // PDO [PHP]
 
-// Naudojant PDO ištraukite visus vartotojus su "admin" privilegijomis.
+// Naudojant PDO ištraukite 13 naujausiu pilnamečių vartotojų.
 
 // P.S.
 // https://github.com/nonamez/CAFS2/tree/master/BackEnd/src/MySQL/2022-10-03/pdo/sql
@@ -12,9 +12,10 @@ try {
     $pdo = new PDO('mysql:host=mysql;dbname=' . getenv('MYSQL_DATABASE'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'));
     
     $queryGetAdmin = 'SELECT * FROM `users` WHERE `is_admin` = :is_admin';
+    $isAdmin = 1;
 
     $stmt = $pdo->prepare($queryGetAdmin);
-    $stmt->execute(['is_admin' => 1]);
+    $stmt->execute(['is_admin' => $isAdmin]);
     $admins = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     var_dump($admins);
