5fb93b4da314a67b02aaab52f92231a6f54b4b95
GedasFS <gedas.fs@gmail.com>
2022-10-04 19:30:39 +0300

    PHP tasks, PDO, adult users, limit 13

---
+++ BackEnd/src/week23/tasks/PDO_users/PdoAdultUsers.php
@@ -0,0 +1,31 @@
+<?php
+
+// PDO [PHP]
+
+// Naudojant PDO ištraukite 13 naujausiu pilnamečių vartotojų.
+
+// P.S.
+// https://github.com/nonamez/CAFS2/tree/master/BackEnd/src/MySQL/2022-10-03/pdo/sql
+
+try {
+
+    $pdo = new PDO('mysql:host=mysql;dbname=' . getenv('MYSQL_DATABASE'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'));
+    
+    // SELECT * FROM `users` WHERE TIMESTAMPDIFF(YEAR, `birthdate`, NOW()) > 18 ORDER BY `id` DESC LIMIT 13;
+
+    $timeDiff = 18;
+    $limitBy = 13;
+
+    $queryNewestAdultUsers = 'SELECT * FROM `users` WHERE TIMESTAMPDIFF(YEAR, `birthdate`, NOW()) > :timeDiff ORDER BY `id` DESC';
+    $queryNewestAdultUsers .= " LIMIT {$limitBy}";
+
+    $stmt = $pdo->prepare($queryNewestAdultUsers);
+    $stmt->execute(['timeDiff' => $timeDiff]);
+    $adultUsers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($adultUsers);
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
